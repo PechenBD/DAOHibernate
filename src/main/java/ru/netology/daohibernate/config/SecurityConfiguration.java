ec06a8f37c66b9a74ce53d87fc6796beccc615bc
Kirill <kirill-11@yandex.ru>
2023-12-26 21:50:37 +0300

    security methods are added

--- src/main/java/ru/netology/daohibernate/config/SecurityConfiguration.java
+++ src/main/java/ru/netology/daohibernate/config/SecurityConfiguration.java
@@ -1,39 +1,35 @@
 package ru.netology.daohibernate.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("Anton")
-                .password("password1")
-                .authorities("read", "write")
+                .password(encoder().encode("password1"))
+                .roles("READ", "WRITE", "DELETE")
                 .and()
                 .withUser("Dmitry")
-                .password("password2")
-                .authorities("write")
+                .password(encoder().encode("password2"))
+                .roles("WRITE")
                 .and()
                 .withUser("Anna")
-                .password("password3")
-                .authorities("read");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/persons/age-less-than").hasAuthority("read")
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-name-and-surname").hasAuthority("write")
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
+                .password(encoder().encode("password3"))
+                .roles("READ");
     }
 }
--- src/main/java/ru/netology/daohibernate/controller/Controller.java
+++ src/main/java/ru/netology/daohibernate/controller/Controller.java
@@ -1,6 +1,9 @@
 package ru.netology.daohibernate.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.netology.daohibernate.entity.Persons;
 import ru.netology.daohibernate.service.Service;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,18 +21,27 @@ import java.util.Optional;
 public class Controller {
     private final Service service;
 
+    @Secured({"ROLE_READ"})
     @GetMapping("/by-city")
     public List<Persons> getPersons(@RequestParam String city) {
         return service.getPersonsByCity(city);
     }
 
+    @RolesAllowed({"ROLE_WRITE"})
     @GetMapping("/age-less-than")
     public List<Persons> getPersonsByAgeLessThanOrderByAge(@RequestParam int age) {
         return service.getPersonsByAgeLessThanOrderByAge(age);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_WRITE', 'ROLE_DELETE')")
     @GetMapping("/by-name-and-surname")
     public Optional<Persons> getPersonsByNameAndSurname(@RequestParam String name, @RequestParam String surname) {
         return service.getPersonsByNameAndSurname(name, surname);
+    }
+
+    @GetMapping("/hello")
+    @PostAuthorize("#username == authentication.principal.username")
+    public String getHello(String username) {
+        return "Hello, " + username;
     }
 }
